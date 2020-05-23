9cde760c09352713026e75350f218afd5fd1ea59
DanielBaker13 <danielbakerr13@gmail.com>
2020-05-23 14:41:36 -0400

    testing for deploy

--- src/components/Front/Pages/Landing/index.js
+++ src/components/Front/Pages/Landing/index.js
@@ -15,6 +15,7 @@ class Home extends Component {
                     <HeroSection />
                     <ClientLogos />
                     <ScreenSlider />
+                    
                     <Features />
                     {/* <PricingTable /> */}
                     {/* <Testimonials /> */}
